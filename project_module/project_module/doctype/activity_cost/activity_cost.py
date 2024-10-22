d5d9d4561e07dc154d9b8c662de8e25464d7645d
ishaq <ishaq.quttaineh@fcuskm.com>
2024-10-22 15:06:54 +0300

    Initial commit for custom app

--- project_module/project_module/doctype/activity_cost/activity_cost.py
+++ project_module/project_module/doctype/activity_cost/activity_cost.py
@@ -6,4 +6,20 @@ from frappe.model.document import Document
 
 
 class ActivityCost(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		activity_type: DF.Link
+		billing_rate: DF.Currency
+		costing_rate: DF.Currency
+		department: DF.Link | None
+		employee: DF.Link | None
+		employee_name: DF.Data | None
+		title: DF.Data | None
+	# end: auto-generated types
 	pass
