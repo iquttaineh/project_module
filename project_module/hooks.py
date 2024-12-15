8945529de1517f3d51092be95540789f1a0591e4
ishaq <ishaq.quttaineh@focuskm.com>
2024-12-15 12:38:11 +0300

    with data

--- project_module/hooks.py
+++ project_module/hooks.py
@@ -241,5 +241,14 @@ web_include_css = "/MyPage/wsr_styles.css"
 # }
 
 fixtures = [
-    
+    "Project Charter", "Meeting", "Meeting Schedule", "Attendees", "Members", 
+    "Project Members", "Stakeholders", "Risk Levels Child", "Risk Levels", "Issues Log", 
+    "Risks Log", "Actions", "Constraints", "Business Need", "Stakeholder Party", 
+    "Payment Plan", "Stakeholder Type", "Payment Against", "Project Budget Category", "Milestones", 
+    "Risks", "Project Budget", "Payment Type", "Activity Cost", "Penalties", 
+    "SLA Approval", "SLA Versions", "Validity of Agreement", "Service Performance Metrics", "Conditions of Cancellation", 
+    "Excluded Services", "Service Availability", "Service Assumptions", "Service Provider Requirements", "Customer Requirements", 
+    "Service Scope", "Goals and Objectives", "Project Objectives", "Key Deliverables", "Project Scope", 
+    "DjangoProperty", 
+    "Project", "Task", "Project Type"
 ]
